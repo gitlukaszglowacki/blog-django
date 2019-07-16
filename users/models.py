f5064f1aac8091b13513ecce58816b835fd18e41
Łukasz Głowacki <mrlukaszglowacki@gmail.com>
2019-07-16 23:21:58 +0200

    Pics on AWS S3, creds in local env

--- users/models.py
+++ users/models.py
@@ -1,21 +1,30 @@
+import io
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.core.files.storage import default_storage as storage
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'Profil {self.user.username}'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img_read = storage.open(self.image.name, 'r')
+        img = Image.open(img_read)
 
         if img.height > 300 or img.width > 300:
-            output_size(300, 300)
+            output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            in_mem_file = io.BytesIO()
+            img.save(in_mem_file, format='JPEG')
+            img_write = storage.open(self.image.name, 'w+')
+            img_write.write(in_mem_file.getvalue())
+            img_write.close()
 
+        img_read.close()
